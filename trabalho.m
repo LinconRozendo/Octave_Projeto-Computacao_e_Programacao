76ceba614b01ceff95713c0297cdcf35344ab48b
Lincon Rozendo <70492451+LinconRozendo@users.noreply.github.com>
2020-08-30 23:33:08 -0300

    Update trabalho.m
    Código em Octave feito para derivar funções pré-determinadas pelo professor da disciplina Computação e Programação.

--- trabalho.m
+++ trabalho.m
@@ -10,20 +10,20 @@ syms x1;
 
 figure(1);
 plot(x,y,"-r",'LineWidth',3);
-title("Gráfico de 2 + sen(x-8).");
+title("Grå¡»ico de 2 + sen(x-8).");
 xlabel("Eixo X");
 ylabel("Eixo Y");
 
 figure(2);
 plot(x,y1, "-b", 'LineWidth', 3);
-title("Gráfico de ³V¯x.");
+title("Grå¡»ico de è¦“çŸ©.");
 xlabel("Eixo X");
 ylabel("Eixo Y");
 
 figure(3);
 plot(x, y,"-r",'LineWidth',2, x, y1, "-b", 'LineWidth', 4);
-title("Gráfico das funções 2 + sen(x-8) e ³V¯x.");
-legend('2 + sen(x-8) em vermelho.', '³V¯x em azul.');
+title("Grå¡»ico das funè¸¥es 2 + sen(x-8) e è¦“çŸ©.");
+legend('2 + sen(x-8) em vermelho.', 'è¦“çŸ© em azul.');
 xlabel("Eixo X");
 ylabel("Eixo Y");
 
@@ -31,9 +31,9 @@ f1 = 2+sin(x1-8);
 f2 = cbrt(x1);
 
 diff(f1);
-printf("\nResultado da derivada da função 2+sen(x-8): \n");
+printf("\nResultado da derivada da funèª½o 2+sen(x-8): \n");
 ans
-printf("\nResultado da derivada da função ³V¯x(raíz cúbica de x) : \n");
+printf("\nResultado da derivada da funèª½o è¦“çŸ©(raçžµ cî€¢ica de x) : \n");
 diff(f2);
 ans
 
@@ -42,14 +42,15 @@ f2 = 1/3*x.^(2/3);
 
 figure(4);
 plot(x,y,"-r",'LineWidth',2,x,f1, "-b", 'LineWidth',4);
-title("Gráfico da função 2 + sen(x-8) e sua derivada.");
+title("Grå¡»ico da funèª½o 2 + sen(x-8) e sua derivada.");
 legend('2 + sen(x-8) em vermelho.', 'cos(x-8) em azul.');
 xlabel("Eixo X");
 ylabel("Eixo Y");
 
 figure(5);
 plot(x,y1,"-r",'LineWidth',2,x,f2, "-b", 'LineWidth',4);
-title("Gráfico da função ³V¯x e sua derivada.");
-legend('³V¯x em vermelho.', '1/3x²³ em azul.');
+title("Grå¡»ico da funèª½o è¦“çŸ© e sua derivada.");
+legend('è¦“çŸ© em vermelho.', '1/3xçœ¾ em azul.');
 xlabel("Eixo X");
 ylabel("Eixo Y");
+
